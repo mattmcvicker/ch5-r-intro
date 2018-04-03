fc25db65d7db803d700189f2da2d96dea55d3567
Matthew Mcvicker <23mcvicker@gmail.com>
2018-04-02 20:26:26 -0700

    Assignment participation

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -4,19 +4,19 @@
 hometown <- Benicia
 
 # Assign your name to the variable `my_name`
-my_name <- mAtthew
+my_name <- Matthew
 
 # Assign your height (in inches) to a variable `my_height`
-
+my_height <- 5
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
-
+puppies <- 6
 
 # Create a variable `puppy_price`, which is how much you think a puppy costs
-
+puppy_price <- 99999999999
 
 # Create a variable `total_cost` that has the total cost of all of your puppies
-
+total_cost <- puppy_price * puppies
 
 # Create a boolean variable `too_expensive`, set to TRUE if the cost is greater 
 # than $1,000
